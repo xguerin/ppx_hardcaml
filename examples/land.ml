3acf97a234764247034bcbc621b05e5bfe7adf17
Xavier Guérin <github@applepine.org>
2016-12-02 20:08:18 -0500

    Add support for nested Pexp_apply, .[,] operator

--- examples/land.ml
+++ examples/land.ml
@@ -1,4 +1,15 @@
 let v0 a b = [%hw a lor  b];;
 let v1 a b = [%hw a land b];;
 let v2 a b = [%hw a lxor b];;
-let v3 a   = [%hw   lnot a];;
+let v3     = [%hw   lnot a];;
+
+let some_fun a b =
+  let i0 = [%hw a lor  b]
+  and i1 = [%hw a land b]
+  and i2 = [%hw a lxor b]
+  and i3 = [%hw   lnot a] in
+  [%hw i0 land (i1 land i2) ]
+
+let subscript_fun signal =
+  let%hw subsr = signal.[1,2] in
+  subsr
--- src/ppx_hardcaml.ml
+++ src/ppx_hardcaml.ml
@@ -29,6 +29,18 @@ let location_exn ~loc msg =
   |> raise
 ;;
 
+(* Wrappers *)
+
+let wrap_expr ~loc { pexp_desc; pexp_loc; pexp_attributes } =
+  let ext = Location.mkloc "hw" loc in
+  let evl = Pstr_eval({ pexp_desc; pexp_loc; pexp_attributes }, pexp_attributes) in 
+  let str = PStr([{ pstr_desc = evl ; pstr_loc = pexp_loc }])
+  in
+  { pexp_desc = Pexp_extension(ext, str); pexp_loc; pexp_attributes }
+
+let wrap_let_binding ~loc { pvb_pat; pvb_expr; pvb_loc; pvb_attributes } =
+  { pvb_pat; pvb_expr = wrap_expr ~loc pvb_expr; pvb_loc; pvb_attributes }
+
 (* Binary operators conversion *)
 
 let to_hw_ident_lident ~loc = function
@@ -52,27 +64,38 @@ let to_hw_ident_lident ~loc = function
 
 let to_hw_ident ~loc = function
   | { txt = Lident(strn); loc } -> to_hw_ident_lident ~loc strn
+  | { txt = Ldot(Lident("String"), "get"); loc } -> { txt = Lident("select"); loc }
   | _ -> location_exn ~loc "Invalid use of HardCaml PPX extension"
 
 (* Scenarios *)
 
-let do_apply ~loc = function
-  | { pexp_desc = Pexp_ident(ident); pexp_loc; pexp_attributes } ->
+let rec do_apply ~loc { pexp_desc; pexp_loc; pexp_attributes } = 
+  match pexp_desc with
+  | Pexp_apply({ pexp_desc = Pexp_ident(ident); pexp_loc; pexp_attributes }, ops) ->
+    let hw_ops   = List.map (fun (l, e) -> (l, do_apply ~loc e)) ops in
     let hw_ident = to_hw_ident ~loc ident in
-    { pexp_desc = Pexp_ident(hw_ident); pexp_loc; pexp_attributes }
-  | _ -> location_exn ~loc "Invalid use of HardCaml PPX extension"
+    let hw_label = { pexp_desc = Pexp_ident(hw_ident); pexp_loc; pexp_attributes } in
+    { pexp_desc = Pexp_apply(hw_label, hw_ops); pexp_loc; pexp_attributes }
+  | _ -> { pexp_desc; pexp_loc; pexp_attributes }
+
+let do_let ~loc bindings =
+  List.map (wrap_let_binding ~loc) bindings
 
 (* Mapper *)
 
 open Ppx_core.Std
 
 let mapper ~loc ~path:_ { pexp_desc; pexp_loc; pexp_attributes } =
+  printf "%s\n" (Pprintast.string_of_expression { pexp_desc; pexp_loc; pexp_attributes });
   match pexp_desc with
-  | Pexp_apply(label, ops) ->
-    { pexp_desc = Pexp_apply(do_apply ~loc label, ops); pexp_loc; pexp_attributes }
+  | Pexp_apply(_, _) ->
+    do_apply ~loc { pexp_desc; pexp_loc; pexp_attributes }
+  | Pexp_let(Nonrecursive, bindings, next) ->
+    let wb = do_let ~loc bindings in
+    { pexp_desc = Pexp_let(Nonrecursive, wb, next) ; pexp_loc; pexp_attributes }
   | _ -> location_exn ~loc "Invalid use of HardCaml PPX extension"
 
-let extension =
+let expr_extension =
   Extension.V2.declare
     "hw"
     Extension.Context.expression
@@ -81,5 +104,5 @@ let extension =
 ;;
 
 let () =
-  Ppx_driver.register_transformation "hw" ~extensions:[extension]
+  Ppx_driver.register_transformation "hw" ~extensions:[expr_extension]
 ;;
